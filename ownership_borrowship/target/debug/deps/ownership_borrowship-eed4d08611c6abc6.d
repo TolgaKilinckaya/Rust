ca002ef39869038447867f4874645d22188b5530
TolgaKilinckaya <tolgakilinckaya@gmail.com>
2024-04-04 17:16:19 +0300

    create ownership

---
+++ ownership_borrowship/target/debug/deps/ownership_borrowship-eed4d08611c6abc6.d
@@ -0,0 +1,5 @@
+C:\Users\tolgakilinckaya\Desktop\Rust\ownership_borrowship\target\debug\deps\ownership_borrowship-eed4d08611c6abc6.exe: src\main.rs
+
+C:\Users\tolgakilinckaya\Desktop\Rust\ownership_borrowship\target\debug\deps\ownership_borrowship-eed4d08611c6abc6.d: src\main.rs
+
+src\main.rs:
---
+++ ownership_borrowship/target/debug/ownership_borrowship.d
@@ -0,0 +1,1 @@
+C:\Users\tolgakilinckaya\Desktop\Rust\ownership_borrowship\target\debug\ownership_borrowship.exe: C:\Users\tolgakilinckaya\Desktop\Rust\ownership_borrowship\src\main.rs
